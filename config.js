79325e1cdb28437e57df649b2604cd954af6214a
projectgheist <projectgheist@gmail.com>
2015-11-13 23:18:13 +0000

    [] almost complete codecoverage

--- config.js
+++ config.js
@@ -1,52 +1,23 @@
-/** Application dns location/url
+/** Application port
  */
-module.exports.Url = function () {
-	if (process.env.OPENSHIFT_APP_DNS) { // Openshift
-		return 'http://' + process.env.OPENSHIFT_APP_DNS;
-	} else if (process.env.AF_APP_URL) { // Appfog
-		return process.env.AF_APP_URL;
-	}
-	return 'http://localhost:' + exports.Port();
-};
-
-/** Application directory on server
- */
-module.exports.Dir = function () {
-	// Openshift
-	if (process.env.OPENSHIFT_REPO_DIR) {
-		return process.env.OPENSHIFT_REPO_DIR;
-	}
-	return __dirname + '/';
+module.exports.Port = function () {
+	return process.env.OPENSHIFT_NODEJS_PORT || // Openshift
+		process.env.VCAP_APP_PORT || // Appfog
+		(process.env.PORT || 3000); // Local
 };
 
 /** Application ip address
  */
 module.exports.IpAddr = function () {
-	// Openshift
-	if (process.env.OPENSHIFT_NODEJS_IP) {
-		return process.env.OPENSHIFT_NODEJS_IP;
-	}
-	return '127.0.0.1';
+	return process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 };
 
-/** Application port
+/** Application dns location/url
  */
-module.exports.Port = function () {
-	if (process.env.OPENSHIFT_NODEJS_PORT) { // Openshift
-		return process.env.OPENSHIFT_NODEJS_PORT;
-	} else if (process.env.VCAP_APP_PORT) { // Appfog
-		return process.env.VCAP_APP_PORT;
-	}
-	return (process.env.PORT || 3000);
-};
-
-/** Google OAuth details
- */
-module.exports.Google = function () {
-	return {
-		ClientID: process.env.GOOGLE_CLIENT_ID || 'GOOGLE_CLIENT_ID',
-		ClientSecret: process.env.GOOGLE_CLIENT_SECRET || 'GOOGLE_CLIENT_SECRET'
-	};
+module.exports.Url = function () {
+	return (process.env.OPENSHIFT_APP_DNS ? ['http://', process.env.OPENSHIFT_APP_DNS].join('') : false) || // Openshift
+		process.env.AF_APP_URL || // Appfog
+		[exports.IpAddr(), ':', exports.Port()].join(''); // Local
 };
 
 /** Mongo DB Credentials
@@ -54,22 +25,11 @@ module.exports.Google = function () {
 module.exports.db = function (dbName) {
 	// default database name
 	dbName || (dbName = 'db_timecalendar');
-	// Appfog
-	if (process.env.VCAP_SERVICES) {
-		var env = JSON.parse(process.env.VCAP_SERVICES);
-		// support multiple mongodb configurations
-		if (env['mongodb-1.8']) {
-			return env['mongodb-1.8'][0]['credentials'];
-		} else if (env['mongodb2-2.4.8']) {
-			return env['mongodb2-2.4.8'][0]['credentials'];
-		}
-	} else {
-		return {
-			hostname: process.env.OPENSHIFT_MONGODB_DB_HOST || 'localhost',
-			port: process.env.OPENSHIFT_MONGODB_DB_PORT || '27017',
-			dbname: process.env.OPENSHIFT_APP_NAME || dbName,
-			username: process.env.OPENSHIFT_MONGODB_DB_USERNAME || '',
-			password: process.env.OPENSHIFT_MONGODB_DB_PASSWORD || ''
-		};
-	}
+	return {
+		hostname: process.env.OPENSHIFT_MONGODB_DB_HOST || 'localhost',
+		port: process.env.OPENSHIFT_MONGODB_DB_PORT || '27017',
+		dbname: process.env.OPENSHIFT_APP_NAME || dbName,
+		username: process.env.OPENSHIFT_MONGODB_DB_USERNAME || '',
+		password: process.env.OPENSHIFT_MONGODB_DB_PASSWORD || ''
+	};
 };
--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,6 @@
 /** Includes
  */
 var mg = require('mongoose');
-var vd = require('validator');
 
 /** function isEmpty
 */
@@ -12,57 +11,6 @@ exports.isEmpty = function (obj) {
 		}
 	}
 	return true;
-};
-
-/** function startsWith
- * Checks if a string starts with a certain char/string
- * @param val: char or string to search for in the input string
- * @param str: input string/array of strings
- * @param out: string value that starts with the defined val
-*/
-exports.startsWith = function (val, str, out) {
-	// check if already an array, else make it an array
-	if (!Array.isArray(str)) {
-		str = [str];
-	}
-	// loop array of strings
-	for (var i in str) {
-		if (val.indexOf(str[i].toLowerCase()) === 0) {
-			out = str[i];
-			return true;
-		}
-	}
-	return false;
-};
-
-/** function stringInsert
-@param o: original string
-@param i: string to insert
-@param p: insert position
-*/
-exports.stringInsert = function (o, i, p) {
-	return [o.slice(0, p), i, o.slice(p)].join('');
-};
-
-exports.stringReplace = function (val, str) {
-	// check if already an array, else make it an array
-	if (!Array.isArray(str)) {
-		str = [str];
-	}
-	var re;
-	for (var i in str) {
-		re = new RegExp('(' + str[i] + ')(?:.*?)', 'i');
-		val = val.replace(re, parseInt(i, 0) + 1);
-	}
-	return val;
-};
-
-exports.clamp = function (val, min, max) {
-	return Math.min(Math.max(val, min), max);
-};
-
-exports.fullURL = function (req) {
-	return req.protocol + '://' + req.headers.host + req.url;
 };
 
 exports.ref = function (type) {
@@ -81,44 +29,14 @@ exports.refAndRequired = function (type) {
 	};
 };
 
-// check if the string is a url
-exports.isUrl = function (url) {
-	return vd.isURL(url);
-};
-
-// checks if value is an array
-exports.isArray = function (val) {
-	return Array.isArray(val);
-};
-
 // returns a string that points to the database url
 exports.getDBConnectionURL = function (obj, noPrefix) {
-	var r = '';
-	if (process.env.OPENSHIFT_MONGODB_DB_URL) {
-		r = process.env.OPENSHIFT_MONGODB_DB_URL + obj.dbname;
-	} else if (obj.url) {
-		r = obj.url;
-	} else if (obj.username && obj.password) {
-		r = 'mongodb://' + obj.username + ':' + obj.password + '@' + obj.hostname + ':' + obj.port + '/' + obj.dbname;
-	} else {
-		r = 'mongodb://' + obj.hostname + ':' + obj.port + '/' + obj.dbname;
+	var r = process.env.OPENSHIFT_MONGODB_DB_URL ? [process.env.OPENSHIFT_MONGODB_DB_URL, obj.dbname].join('') : obj.url;
+	if (!r) {
+		r = (obj.username && obj.password) ? [obj.username, ':', obj.password, '@'].join('') : '';
+		r = [r, obj.hostname, ':', obj.port, '/', obj.dbname].join('');
 	}
-	if (r) {
-		r = r.substring(10, r.length);
-	}
+	// removes 'mongodb://' from string
+	r = r.replace(/mongodb:\/\//gi, '');
 	return r;
-};
-
-/** function parseHtmlEntities
-*/
-exports.parseHtmlEntities = function (str) {
-	if (!str) {
-		return '';
-	}
-	return str
-		.replace(/&#([0-9]{1,3});/gi, function (match, numStr) {
-			var num = parseInt(numStr, 10);// read num as normal number
-			return String.fromCharCode(num);
-		})
-		.trim();
 };
